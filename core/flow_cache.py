3787407f01236fcb0ae359f8084e7c0dd698ef16
zeffii <tetha.z@gmail.com>
2014-10-11 15:18:12 +0200

    first cache implementation

---
+++ core/flow_cache.py
@@ -0,0 +1,39 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+flowcache = {}
+
+
+def ident(socket):
+    node_name = socket.from_node.name
+    socket_name = socket.from_socket.name
+    return node_name, socket_name
+
+
+def cache_wipe():
+    global flowcache
+    flowcache = {}
+
+
+def cache_put(socket, data):
+    global flowcache
+    flowcache[ident(socket)] = data
+
+
+def cache_get(socket):
+    return flowcache.get(ident(socket), [])
