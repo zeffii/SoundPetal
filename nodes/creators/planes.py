2beb40b283cd19654b00596893106b5744317252
zeffii <tetha.z@gmail.com>
2014-10-24 17:28:31 +0200

    hardcode plane, when possible :)

--- nodes/creators/planes.py
+++ nodes/creators/planes.py
@@ -16,24 +16,66 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+import numpy as np
+
 import bpy
-from bpy.props import BoolProperty, BoolVectorProperty
+from bpy.props import EnumProperty, FloatProperty
 
 from FLOW.core.mechanisms import updateSD
 from FLOW.node_tree import FlowCustomTreeNode
+
+
+def make_geometry(self, s):
+    half = s / 2.0
+    axis = self.axis
+
+    v = []
+    if axis == 'X':
+        v = [[0, -s, -s], [0, s, -s], [0, s, s], [0, -s, s]]
+    elif axis == 'Y':
+        v = [[-s, 0, -s], [s, 0, -s], [s, 0, s], [-s, 0, s]]
+    else:
+        v = [[-s, -s, 0], [s, -s, 0], [s, s, 0], [-s, s, 0]]
+
+    verts = np.array(v)
+    faces = np.array([np.arange(4)])
+    return verts, faces
 
 
 class FlowPlanesNode(bpy.types.Node, FlowCustomTreeNode):
     ''' FlowPlanesNode '''
     bl_idname = 'FlowPlanesNode'
     bl_label = 'Planes'
-    bl_icon = 'OUTLINER_OB_EMPTY'
+
+    Side = FloatProperty(name='Side', update=updateSD)
+
+    axis_options = [
+        ("X", "X", "", 0),
+        ("Y", "Y", "", 1),
+        ("Z", "Z", "", 2)
+    ]
+
+    axis = EnumProperty(
+        items=axis_options,
+        name="Type of axis",
+        description="offers plane X|Y|Z",
+        default="Z",
+        update=updateSD)
 
     def init(self, context):
-        self.outputs.new('FlowSinkHoleSocket', "send")
+        self.inputs.new("FlowScalarSocket", "Side").prop_name = "Side"
+        self.outputs.new("FlowArraySocket", "verts")
+        self.outputs.new("FlowArraySocket", "faces")
+
+    def draw_buttons(self, context, layout):
+        row = layout.row()
+        row.prop(self, 'axis', expand=True)
 
     def process(self):
-        pass
+        s = self.inputs['Side'].fget2()
+        verts, faces = make_geometry(self, s)
+        self.outputs[0].fset(verts)
+        self.outputs[1].fset(faces)
 
 
 def register():
